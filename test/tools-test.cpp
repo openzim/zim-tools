81051b9c563a6b4cd96a686155aa1770d4937502
MiguelRocha <miguelrocha@ua.pt>
2020-08-16 14:40:37 +0100

    Add unit test for tools

--- test/tools-test.cpp
+++ test/tools-test.cpp
@@ -167,3 +167,59 @@ TEST(CommonTools, getNamespaceForMimeType)
   EXPECT_EQ(getNamespaceForMimeType("image/png", false), "I");
   EXPECT_EQ(getNamespaceForMimeType("application/json", false), "-");
 }
+
+TEST(tools, isOutofBounds)
+{
+    ASSERT_FALSE(isOutofBounds("", ""));
+    ASSERT_TRUE(isOutofBounds("../../..", ""));
+    ASSERT_TRUE(isOutofBounds("../", ""));
+    ASSERT_FALSE(isOutofBounds("../", "/a/b"));
+    ASSERT_FALSE(isOutofBounds("../", "/a"));
+    ASSERT_TRUE(isOutofBounds("../../", "/a"));
+}
+
+TEST(tools, normalize_link)
+{
+    ASSERT_EQ(normalize_link("/a", "/b"), "a");
+
+    // not absolute
+    ASSERT_EQ(normalize_link("a", "/b"), "/b/a");
+    ASSERT_EQ(normalize_link("../a", "/b/c"), "/b/a");
+    ASSERT_EQ(normalize_link(".././a", "/b/c"), "/b/a");
+    ASSERT_EQ(normalize_link("../a/b/aa#localanchor", "/b/c"), "/b/a/b/aa");
+    ASSERT_EQ(normalize_link("../a/b/aa?localanchor", "/b/c"), "/b/a/b/aa");
+}
+
+TEST(tools, addler32)
+{
+    ASSERT_EQ(adler32("sdfkhewruhwe8"), 640746832);
+    ASSERT_EQ(adler32("sdifjsdf"), 251593550);
+    ASSERT_EQ(adler32("q"), 7471218);
+    ASSERT_EQ(adler32(""), 1);
+}
+
+TEST(tools, getLinks)
+{
+    auto v = generic_getLinks("");
+
+    ASSERT_TRUE(v.empty());
+
+    std::string page1 = "<link href=\"https://fonts.goos.com/css?family=OpenSans\" rel=\"stylesheet\">";
+    auto v1 = generic_getLinks(page1);
+
+    ASSERT_FALSE(v1.empty());
+    ASSERT_TRUE(v1.size() == 1);
+    ASSERT_EQ(v1[0], "https://fonts.goos.com/css?family=OpenSans");
+
+    std::string page2 = "<link href=\"https://fonts.goos.com/css?family=OpenSans\" rel=\"stylesheet\">";
+    auto v2 = generic_getLinks(page2, false);
+
+    ASSERT_TRUE(v2.empty());
+
+    std::string page3 = "<link src=\"https://fonts.goos.com/css?family=OpenSans\" rel=\"stylesheet\">";
+    auto v3 = generic_getLinks(page3);
+
+    ASSERT_FALSE(v3.empty());
+    ASSERT_TRUE(v3.size() == 1);
+    ASSERT_EQ(v3[0], "https://fonts.goos.com/css?family=OpenSans");
+}
