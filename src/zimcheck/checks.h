f01c9f5f8de4594b9fe0b5ee30cb5488339d9afc
Pranavjeet-Naidu <pranavjeetnaidu@gmail.com>
2026-05-19 06:04:05 +0530

    optimize: modify logging from batching to streaming
    Signed-off-by: Pranavjeet-Naidu <pranavjeetnaidu@gmail.com>

--- src/zimcheck/checks.h
+++ src/zimcheck/checks.h
@@ -78,8 +78,7 @@ class ErrorLogger {
       MsgParams msgParams;
     };
 
-    // reportMsgs[i] holds messages for the i'th test/check
-    std::vector<std::vector<MsgIdWithParams>> reportMsgs;
+    bool logStreamOpen = false;
 
     // testStatus[i] corresponds to the status of i'th test
     std::bitset<size_t(TestType::COUNT)> testStatus;
@@ -97,6 +96,9 @@ class ErrorLogger {
 
     void infoMsg(const std::string& msg) const;
 
+    void startLogStream();
+    void endLogStream();
+
     template<class T>
     void addInfo(const std::string& key, const T& value) {
       if ( jsonOutputStream.enabled() ) {
@@ -106,7 +108,6 @@ class ErrorLogger {
 
     void setTestResult(TestType type, bool status);
     void addMsg(MsgId msgid, const MsgParams& msgParams);
-    void report(bool error_details) const;
     bool overallStatus() const;
 };
 
